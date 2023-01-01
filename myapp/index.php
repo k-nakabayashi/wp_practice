29a6e9578d2a2a57ff1edfafb1bbf04bec9a09ab
nakabayashi kotaro <123windam@gmail.com>
2023-01-01 20:42:46 +0900

    デバッグログ出力関数を編集

--- myapp/index.php
+++ myapp/index.php
@@ -12,8 +12,16 @@
 // require_once "api.php"; // json
  
 
-function debug_log($message) {
+function debug_log($message, $display_timestamp=true) {
+
     // WP_CONTENT_DIR = /var/www/html/wp-content
-    $log_message = sprintf("%s:%s\n", date_i18n('Y-m-d H:i:s'), $message);
+    $log_message = "";
+
+    if ($display_timestamp == true) {
+        $log_message = sprintf("%s:%s\n", date_i18n('Y-m-d H:i:s'), $message);
+    } else {
+        $log_message = $message;
+    }
+
     error_log($log_message, 3, WP_CONTENT_DIR . WP_DEBUG_LOG_PATH);
 }
